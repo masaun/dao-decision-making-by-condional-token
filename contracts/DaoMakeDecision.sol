7de549b2e4ba3c1b0511124804ba23643b1b23c3
masaun <masanoriuno@gmail.com>
2020-08-04 08:30:08 +0900

    Add AddMemberToOrganization()

--- contracts/DaoMakeDecision.sol
+++ contracts/DaoMakeDecision.sol
@@ -1,12 +1,19 @@
 pragma solidity ^0.5.1;
 
 import "./Storage/DaoMakeDecision/Objects.sol";
-
 import './GnosisPmSystem.sol';
 
 
 contract DaoMakeDecision {
-	
-    /// 
+	address[] public organizationMembers;
+    mapping(address => string) public memberNames;
 
+    /***
+     * @notice - Add make decision as DAO
+     **/
+    function AddMemberToOrganization(address member, string memberName) public returns (bool) {
+        organizationMembers.push(member);
+        memberNames[member] = memberName;
+    }
+    
 }
