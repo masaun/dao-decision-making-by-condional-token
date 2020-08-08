f5e8807ca8ae77e885770d8440c9f32e122d255b
masaun <masanoriuno@gmail.com>
2020-08-08 10:01:39 +0900

    Replace questionId with questionId which is called from martkets.config.js

--- test/dao-make-decision.js
+++ test/dao-make-decision.js
@@ -4,6 +4,11 @@ const chai = require('chai')
 const assert = chai.assert
 chai.use(require('chai-as-promised'))
 
+/// Market Info
+const marketConfig = require('../markets.config.js')
+console.log('=== marketConfig ===', marketConfig);
+console.log('=== marketConfig[0].questionId ===', marketConfig[0].questionId);
+
 
 contract('DaoMakeDecision', accounts => {
     let daoMakeDecision
@@ -31,7 +36,8 @@ contract('DaoMakeDecision', accounts => {
 
     it('execute _prepareCondition()', async () => {
         const oracle = web3.utils.randomHex(20); 
-        const questionId = web3.utils.randomHex(32);
+        const questionId = marketConfig[0].questionId;
+        //const questionId = web3.utils.randomHex(32);
         const outcomeSlotCount = web3.utils.toBN(256);
 
         const condition = await daoMakeDecision._prepareCondition(oracle, questionId, outcomeSlotCount)
